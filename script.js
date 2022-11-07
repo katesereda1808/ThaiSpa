569799a08613cfa6ce2cebd8e5d0f2e03aa44697
katesereda1808 <katesereda22@gmail.com>
2022-11-07 15:00:57 +0300

    cursor pointer added

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
+"use strict";
 const mob_menu = document.querySelector('.menu_mobile');
 const close_mob_menu = document.querySelector('.close_btn');
 const open_mob_menu = document.querySelector('.burger_mobile')
 console.log(open_mob_menu);
 close_mob_menu.addEventListener('click', ()=>{
     mob_menu.classList.add('hide');
-    console.log(mob_menu.classList)
 })
 open_mob_menu.addEventListener('click', ()=>{
     mob_menu.classList.remove('hide');
-    console.log(mob_menu.classList)
 })
